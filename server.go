0d1a0098ddc4b1a7ee465a639fc177ed870410be
Rinc <i@RincLiu.com>
2014-06-18 16:29:55 +0800

    Modified visibility of Handler.

--- server.go
+++ server.go
@@ -5,25 +5,25 @@ import (
 	"net/http"
 )
 
-type Handler struct{
+type handler struct{
 	api *API
 }
 
-func (handler *Handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func (h *handler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	fmt.Printf("Request: %s From: %s\n", request.URL, request.RemoteAddr)	
 	switch request.URL.Path {
 		case "/api/book/search":
-			handler.api.SearchBook(writer, request)
+			h.api.SearchBook(writer, request)
 		case "/api/movie/search":
-			handler.api.SearchMovie(writer, request)
+			h.api.SearchMovie(writer, request)
 		case "/api/music/search":
-                        handler.api.SearchMusic(writer, request)
+                        h.api.SearchMusic(writer, request)
 		default:
 			http.NotFound(writer, request)
 	}
 }
 
 func Start() {
-	err := http.ListenAndServe(":32768", &Handler{ &API{} })
+	err := http.ListenAndServe(":32768", &handler{ &API{} })
 	CheckErr(err)
 }
