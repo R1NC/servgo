c5642f9c7305cc6b2eeb2dc4c111d9d57ff27ab1
Rinc <i@RincLiu.com>
2014-08-19 06:33:43 -0400

    Modified redis operations.

--- redis.go
+++ redis.go
@@ -10,19 +10,20 @@ type Redis struct {}
 func (rds *Redis) QueryCityIdByName(name string) string {
 	conn := getConn()
 	exists, err := redis.Bool(conn.Do("EXISTS", name))
+	CheckErr(err)
 	fmt.Printf("CityName(%s) exists:%#v\n", name, exists)
 	if exists {
 		cityId, err := redis.String(conn.Do("GET", name))
-		checkErr(err)
+		CheckErr(err)
 		fmt.Printf("CityId:%s\n", cityId)
 		return cityId
 	}
 	return ""
 }
 
-func getConn() Conn {
-	conn, err := redis.Dial("tcp", "localhost:6397")
-	checkErr(err)
+func getConn() redis.Conn {
+	conn, err := redis.Dial("tcp", "localhost:6379")
+	CheckErr(err)
 	defer conn.Close()
 	return conn
 } 
